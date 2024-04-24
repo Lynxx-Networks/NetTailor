8fd3339952ce7d33b995487218fb03027abe6573
madeofpendletonwool <collinp@collinpendleton.com>
2024-04-24 16:04:50 -0500

    added config generation

--- web/src/components/create_config.rs
+++ web/src/components/create_config.rs
@@ -1,20 +1,121 @@
 use yew::prelude::*;
-use yew_router::prelude::Link;
 use yewdux::use_store;
-use crate::components::context::AppState;
-use super::routes::Route;
-
+use super::app_drawer::App_drawer;
+use super::search_nav::Search_nav;
+use yew_router::history::BrowserHistory;
+use crate::components::context::{AppState, UIState};
+use crate::components::misc_func::generate_config;
 
 #[function_component(CreateConfig)]
 pub fn create_config() -> Html {
-        html! {
-            <>
-                <div class="empty-episodes-container">
-                    <img src="static/assets/favicon.png" alt="Logo" class="logo"/>
-                    <h1>{ "Page not found" }</h1>
-                    <p>{"Sorry for the inconvenience. You could eat a taco to cheer you up :)"}</p>
-                </div>
-            </>
+    let (state, dispatch) = use_store::<AppState>();
+    let effect_dispatch = dispatch.clone();
+    let history = BrowserHistory::new();
+
+
+    let (post_state, _post_dispatch) = use_store::<AppState>();
+    let (audio_state, audio_dispatch) = use_store::<UIState>();
+    let error_message = audio_state.error_message.clone();
+    let info_message = audio_state.info_message.clone();
+    let loading = use_state(|| true);
+ 
+    let os_version = use_state(|| String::from(""));
+    let hostname = use_state(|| String::from(""));
+    let tacacs_server = use_state(|| String::from(""));
+    let ise_server = use_state(|| String::from(""));
+    let timezone = use_state(|| String::from(""));
+    let boot_bin = use_state(|| String::from(""));
+    let switch_number = use_state(|| String::from(""));
+    let model = use_state(|| String::from(""));
+    let vtp_domain = use_state(|| String::from(""));
+    let auvik_collector = use_state(|| String::from(""));
+    let crypto_auth_block = use_state(|| String::from(""));
+    let vlan_range = use_state(|| String::from(""));
+    let encrypted_user_pass = use_state(|| String::from(""));
+    let vlan_definitions = use_state(|| String::from(""));
+    let class_map_definitions = use_state(|| String::from(""));
+    let policy_map_definitions = use_state(|| String::from(""));
+
+
+    let configuration = use_state(|| String::new());
+
+    // Adjust the generate_config_click to accept a MouseEvent parameter
+    let generate_config_click = {
+        let configuration = configuration.clone();
+        let os_version = os_version.clone();
+        let hostname = hostname.clone();
+        let tacacs_server = tacacs_server.clone();
+        let ise_server = ise_server.clone();
+        let timezone = timezone.clone();
+        let boot_bin = boot_bin.clone();
+        let switch_number = switch_number.clone();
+        let model = model.clone();
+        let vtp_domain = vtp_domain.clone();
+        let auvik_collector = auvik_collector.clone();
+        let crypto_auth_block = crypto_auth_block.clone();
+        let vlan_range = vlan_range.clone();
+        let encrypted_user_pass = encrypted_user_pass.clone();
+        let vlan_definitions = vlan_definitions.clone();
+        let class_map_definitions = class_map_definitions.clone();
+        let policy_map_definitions = policy_map_definitions.clone();
+
+        // Define the closure correctly to take a MouseEvent argument
+        Callback::from(move |_: MouseEvent| { // Add _: MouseEvent to the closure parameters
+            let config = generate_config(
+                &os_version,
+                &hostname,
+                &tacacs_server,
+                &ise_server,
+                &timezone,
+                &boot_bin,
+                &switch_number,
+                &model,
+                &vtp_domain,
+                &auvik_collector,
+                &crypto_auth_block,
+                &vlan_range,
+                &encrypted_user_pass,
+                &vlan_definitions,
+                &class_map_definitions,
+                &policy_map_definitions
+            );
+            configuration.set(config.clone());
+            web_sys::console::log_1(&format!("Config: {}", config).into());
+            web_sys::console::log_1(&format!("Config: {}", *configuration).into());
+        })
+    };
+
+    html! {
+        <>
+        <div class="main-container">
+            <Search_nav />
+            {
+                html! {
+                    // <div class="flex flex-col space-y-4 w-full max-w-xs p-8 border border-gray-300 rounded-lg shadow-lg bg-gray-600">
+
+                        <div class="container mx-auto text-center p-10">
+                            <div class="flex justify-center items-center">
+                                <img class="object-scale-down h-20 w-66" src="static/assets/favicon.png" alt="NetTailor Logo" />
+                            </div>
+                            <h1 class="item_container-text text-4xl font-bold mb-5">{"Generate New Configuration"}</h1>
+                            <div>
+                            <textarea class="config-output" value={(*configuration).clone()} readonly=true />
+                            <button onclick={generate_config_click}>{"Generate Config"}</button>
+                            </div>
+                        </div>
+                    // </div>
+                }
+            }
+        // Conditional rendering for the error banner
+        if let Some(error) = error_message {
+            <div class="error-snackbar">{ error }</div>
         }
+        if let Some(info) = info_message {
+            <div class="info-snackbar">{ info }</div>
+        }
+        </div>
+        <App_drawer />
+        </>
+    }
 
 }
--- web/src/components/misc_func.rs
+++ web/src/components/misc_func.rs
@@ -56,3 +56,121 @@ pub fn change_theme(theme: &str) {
         }
     }
 }
+
+pub fn generate_config(
+    os_version: &str,
+    hostname: &str,
+    tacacs_server: &str,
+    ise_server: &str,
+    timezone: &str,
+    boot_bin: &str,
+    switch_number: &str,
+    model: &str,
+    vtp_domain: &str,
+    auvik_collector: &str,
+    crypto_auth_block: &str,
+    vlan_range: &str,
+    encrypted_user_pass: &str,
+    vlan_definitions: &str,
+    class_map_definitions: &str,
+    policy_map_definitions: &str,
+) -> String {
+    format!("
+version {}\n\
+            service timestamps debug datetime msec\n\
+            service timestamps log datetime msec\n\
+            service call-home\n\
+            platform punt-keepalive disable-kernel-core\n\
+            hostname {}\n\
+            vrf definition Mgmt-vrf\n \
+             address-family ipv4\n \
+             exit-address-family\n \
+             address-family ipv6\n \
+             exit-address-family\n\
+            logging console emergencies\n\
+            aaa new-model\n\
+            aaa group server tacacs+ {}\n \
+             server name {}\n \
+             ip tacacs source-interface Loopback254\n\
+            aaa authentication login default group {} local\n\
+            aaa authentication enable default group {} enable\n\
+            aaa session-id common\n\
+            clock timezone {}\n\
+            clock summer-time cdt recurring\n\
+            boot system switch all flash:{}\n\
+            switch {} provision {}\n\
+            ip routing\n\
+            no ip cef optimize neighbor resolution\n\
+            login on-success log\n\
+            udld enable\n\
+            vtp domain {}\n\
+            vtp mode transparent\n\
+            flow record Record-FNF\n \
+             description Flexible NetFlow Monitoring\n \
+             match ipv4 tos\n \
+             match ipv4 protocol\n \
+             match ipv4 source address\n \
+             match ipv4 destination address\n \
+             match transport source-port\n \
+             match transport destination-port\n \
+             collect counter bytes long\n \
+             collect counter packets long\n\
+            flow exporter Export-FNF-Monitor-1\n \
+             description FNFv9 NBAR2 with Auvik\n \
+             destination {}\n \
+             source Loopback254\n \
+             transport udp 2055\n \
+             template data timeout 60\n \
+             option interface-table\n \
+             option application-table\n\
+            flow monitor Monitor-FNF\n \
+             description IWAN Traffc Analysis\n \
+             exporter Export-FNF-Monitor-1\n \
+             cache timeout active 60\n \
+             record Record-FNF\n\
+            authentication mac-move permit\n\
+            table-map AutoQos-4.0-Trust-Dscp-Table\n \
+             default copy\n\
+            table-map policed-dscp\n \
+             map from  0 to 8\n \
+             map from  10 to 8\n \
+             map from  18 to 8\n \
+             map from  24 to 8\n \
+             map from  46 to 8\n \
+             default copy\n\
+            {}\n\
+            license boot level network-essentials addon dna-essentials\n\
+            memory free low-watermark processor 131696\n\
+            diagnostic bootup level minimal\n\
+            spanning-tree mode rapid-pvst\n\
+            spanning-tree extend system-id\n\
+            spanning-tree vlan {} priority 0\n\
+            enable secret 9 {}\n\
+            redundancy\n \
+             mode sso\n\
+            crypto engine compliance shield disable\n\
+            transceiver type all\n \
+             monitoring\n\
+            {}\n\
+            {}\n\
+            {}\n",
+            os_version, 
+            hostname, 
+            tacacs_server, 
+            ise_server, 
+            tacacs_server, 
+            tacacs_server, 
+            timezone, 
+            boot_bin, 
+            switch_number, 
+            model, 
+            vtp_domain, 
+            auvik_collector, 
+            crypto_auth_block, 
+            vlan_range, 
+            encrypted_user_pass, 
+            vlan_definitions, 
+            class_map_definitions, 
+            policy_map_definitions,
+        )
+    }
